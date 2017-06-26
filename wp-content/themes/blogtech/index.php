74dbf83e16ba445559e97d203926d0db0b8d9d22
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-06-26 14:42:31 +0200

    added a 2nd sidebar include (sidebar-section.php) - main.css, bootstrap.css : added style to modal

--- wp-content/themes/blogtech/index.php
+++ wp-content/themes/blogtech/index.php
@@ -14,25 +14,21 @@
           <?php get_template_part( 'content', get_post_format() ); ?>
 
         <?php endwhile; endif; ?>
-        <aside class="asideSection row col-md-12 hidden-sm-down justify-content-between">
-          <img src="<?php echo get_bloginfo('template_directory'); ?>/img/pub/pub1.jpeg" alt="PUB" class="md-4">
-          <img src="<?php echo get_bloginfo('template_directory'); ?>/img/pub/pub2.png" alt="PUB" class="md-4">
-          <img src="<?php echo get_bloginfo('template_directory'); ?>/img/pub/pub3.jpeg" alt="PUB" class="md-4">
-        </aside>
+        <?php get_sidebar('section'); ?>
       </section>
 
       <!-- ################## MODAL ######################### -->
       <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
           <div class="modal-content">
-            <form class="formModal" action="index.html" method="post">
-              <label for="modalname">Username : </label>
-              <input type="text" name="modalname" id="modalname" placeholder="Username">
-              <label for="modaltitle">Title : </label>
-              <input type="text" name="modaltitle" id="modaltitle" placeholder="Title">
-              <label for="modalmessage">Message : </label>
-              <textarea name="modalmessage" id="modalmessage" cols="30" rows="10"></textarea>
-              <div class="modal-footer">
+            <form class="formModal col-12 row justify-content-between" action="index.html" method="post">
+              <label for="modalname" class="col-4">Username : </label>
+              <input type="text" name="modalname" id="modalname" placeholder="Username" class="col-7">
+              <label for="modaltitle" class="col-4">Title : </label>
+              <input type="text" name="modaltitle" id="modaltitle" placeholder="Title" class="col-7">
+              <label for="modalmessage" class="col-4">Message : </label>
+              <textarea name="modalmessage" id="modalmessage" cols="30" rows="10" class="col-7"></textarea>
+              <div class="modal-footer col-12">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary" onclick="confirmA()">Send</button>
               </div>
---
+++ wp-content/themes/blogtech/sidebar-section.php
@@ -0,0 +1,5 @@
+<aside class="asideSection row col-md-12 hidden-sm-down justify-content-between">
+  <img src="<?php echo get_bloginfo('template_directory'); ?>/img/pub/pub1.jpeg" alt="PUB" class="md-4">
+  <img src="<?php echo get_bloginfo('template_directory'); ?>/img/pub/pub2.png" alt="PUB" class="md-4">
+  <img src="<?php echo get_bloginfo('template_directory'); ?>/img/pub/pub3.jpeg" alt="PUB" class="md-4">
+</aside>
